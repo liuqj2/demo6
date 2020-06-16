d96c3324620ca3cd0b0de5cc3d1dd456b0ba1bfe
qijun_liu <qijun_liu@sznoda.com>
2020-06-16 17:19:44 +0800

    dept

---
+++ src/main/java/com/example/demo/Dept.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Dept {
+
+	private String id;
+
+	public Dept(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
